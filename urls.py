72c74563a44d441f418e867ef2ec09aab24d95ed
karabellyj <jozefkarabelly@gmail.com>
2019-10-03 21:45:04 +0200

    add default url

--- urls.py
+++ urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
-
+from django.urls import path
 
 urlpatterns = [
     # add your own patterns here
+    path('', include('core.urls', namespace='core')),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
