9d77253e3c2127788526b0398d0261e7640fa702
jkarabel <jkarabel@redhat.com>
2019-10-07 14:28:52 +0200

    add user_type to User model

--- users/managers.py
+++ users/managers.py
@@ -24,10 +24,13 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('user_type', self.model.USER_TYPES.admin)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('user_type') != self.model.USER_TYPES.admin:
+            raise ValueError('Superuser must have user_type=4).')
 
         return self._create_user(email, password, **extra_fields)
