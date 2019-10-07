4efc3c78c4bfb649aa3734b5dde4fc9eb7a28b44
karabellyj <jozefkarabelly@gmail.com>
2019-10-07 21:17:05 +0200

    move home view and add urls for auth and users

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 app_name = "core"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+
 ]
--- urls.py
+++ urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import url, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # add your own patterns here
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('core.urls', namespace='core')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
