496aa859ee0381421ba880c95f412607fa5fe7d1
karabellyj <jozefkarabelly@gmail.com>
2019-10-09 21:15:36 +0200

    add Product list and create functionality

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,6 @@ app_name = "core"
 urlpatterns = [
     path('tickets/', views.TicketListView.as_view(), name='ticket-list'),
     path('ticket/create/', views.TicketCreateView.as_view(), name='ticket-create'),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django_filters.views import FilterView
 
 from .filters import TicketFilter
-from .models import Ticket
+from .models import Ticket, Product
 
 
 class TicketListView(FilterView):
@@ -15,3 +16,17 @@ class TicketListView(FilterView):
 class TicketCreateView(CreateView):
     model = Ticket
     fields = ('name', 'description', 'product',)
+    success_url = reverse_lazy('core:ticket-list')
+
+
+class ProductListView(FilterView):
+    model = Product
+    paginate_by = 25
+    filterset_fields = ('name',)
+    template_name = 'core/product_list.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'parent',)
+    success_url = reverse_lazy('core:product-list')
