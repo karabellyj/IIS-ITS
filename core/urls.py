1d6903fc54d7fcacf242ec8ec6bb628b482c8f0e
jkarabel <jkarabel@redhat.com>
2019-10-09 20:09:04 +0200

    add ticket creation functionality

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from core.views import TicketListView
+from core import views
 
 app_name = "core"
 urlpatterns = [
-    path('tickets/', TicketListView.as_view(), name='ticket-list'),
+    path('tickets/', views.TicketListView.as_view(), name='ticket-list'),
+    path('ticket/create/', views.TicketCreateView.as_view(), name='ticket-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import CreateView
 from django_filters.views import FilterView
 
 from .filters import TicketFilter
@@ -9,3 +10,7 @@ class TicketListView(FilterView):
     filterset_class = TicketFilter
     paginate_by = 25
     template_name = 'core/ticket_list.html'
+
+
+class TicketCreateView(CreateView):
+    model = Ticket
