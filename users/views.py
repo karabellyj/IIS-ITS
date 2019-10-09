1bd464ef1b914d64a792c19fba21baad99ccb207
jkarabel <jkarabel@redhat.com>
2019-10-09 09:43:51 +0200

    add signup for customers

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
+from .models import User
 from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
     template_name = 'users/signup.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = User.USER_TYPES.customer
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.success_url)
