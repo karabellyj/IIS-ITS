4bfb1eefe42a36d0b777073c0723283a940edee0
karabellyj <jozefkarabelly@gmail.com>
2019-11-26 21:04:57 +0100

    add user update functionality

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, UserListView, UserDetailView, UserCreateView, UserDeleteView
+from .views import SignUpView, UserListView, UserDetailView, UserCreateView, UserDeleteView, UserUpdateView
 
 app_name = 'users'
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('user/create/', UserCreateView.as_view(), name='user-create'),
+    path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('user/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import DetailView
@@ -7,7 +8,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django_filters.views import FilterView
 
 from .filters import UserFilter
-from .models import User
+from .models import User, Lead, Manager, Employee, Customer
 from .forms import CustomerSignUpForm, AllUsersCreateForm
 
 
@@ -48,6 +49,42 @@ class UserCreateView(PermissionRequiredMixin, CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        return redirect(self.success_url)
+
+
+class UserUpdateView(PermissionRequiredMixin, CreateView):
+    model = User
+    fields = ('first_name', 'last_name', 'user_type')
+    success_url = reverse_lazy('users:user-list')
+    permission_required = ('users.change_user',)
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial = initial.copy()
+
+        for field in self.fields:
+            initial[field] = getattr(self.get_object(), field)
+        return initial
+
+    def form_valid(self, form):
+        print('here')
+        if 'user_type' in form.changed_data:
+            user_type_to_cls = {0: Customer, 1: Employee, 2: Manager, 3: Lead, }  # TODO: add
+            user_type_to_group = {0: 'Customers', 1: 'Employees'}  # TODO: add all groups
+
+            self.get_object().groups.clear()
+            group = Group.objects.get(name=user_type_to_group[self.get_object().user_type])
+            self.get_object().groups.add(group)
+
+            user_cls = user_type_to_cls[self.get_object().user_type]
+            user_cls.objects.get(user=self.get_object()).delete()
+
+            new_user_cls = user_type_to_cls[form.cleaned_data['user_type']]
+            new_user_cls.objects.create(user=self.get_object())
+
+        user = User.objects.get(pk=self.get_object().id)
+        user.save(update_fields=form.changed_data)
+
         return redirect(self.success_url)
 
 
