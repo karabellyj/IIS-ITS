887c724bf74f7244cc63b8edfc8665f58c414433
jkarabel <jkarabel@redhat.com>
2019-12-02 08:40:16 +0100

    add new look for user create

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,12 @@
+from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import Group
 from django.db import transaction
+from django.utils.translation import gettext as _
 
-from .utils import get_user_class_by_type, get_user_group_by_type
 from .models import User, Customer
+from .utils import get_user_class_by_type, get_user_group_by_type
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -36,9 +39,28 @@ class CustomerSignUpForm(UserCreationForm):
 
 
 class AllUsersCreateForm(UserCreationForm):
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'uk-input'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'class': 'uk-input'}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
     class Meta(CustomUserCreationForm.Meta):
         model = User
         fields = CustomUserCreationForm.Meta.fields + ('first_name', 'last_name', 'user_type')
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'uk-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'uk-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'uk-input'}),
+            'user_type': forms.Select(attrs={'class': 'uk-select'})
+        }
 
     @transaction.atomic
     def save(self):
